88ec1726e157ee952fbe870810e636c438825ecd
Raimund Kraemer <kraemer_raimund@web.de>
2022-11-29 16:51:17 +0100

    [ABC-42] Print the greeting to the console

--- src/main/java/org/intentionality/Main.java
+++ src/main/java/org/intentionality/Main.java
@@ -3,5 +3,7 @@ package org.intentionality;
 public class Main {
 
     public static void main(String[] args) {
+        String greeting = new Greeting().toString()
+        System.out.println(greeting);
     }
 }
